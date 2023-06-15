12eaccb9a940fe36a8eee9624dd0051970fbda15
diegoforni <d.forni0204@gmail.com>
2023-06-15 02:03:30 -0300

    First Commit (No sé por qué estás viendo esto...)

---
+++ js/main.js
@@ -0,0 +1,52 @@
+(function ($) {
+    "use strict";
+    
+    // loader
+    var loader = function () {
+        setTimeout(function () {
+            if ($('#loader').length > 0) {
+                $('#loader').removeClass('show');
+            }
+        }, 1);
+    };
+    loader();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+    
+    
+
+    
+    
+    // Typed Initiate
+    if ($('.hero .hero-text h2').length == 1) {
+        var typed_strings = $('.hero .hero-text .typed-text').text();
+        var typed = new Typed('.hero .hero-text h2', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 60,
+            backSpeed: 10,
+            smartBackspace: false,
+            loop: false
+        });
+    }
+    
+})(jQuery);
+
+const element = document.getElementById("start");
+element.addEventListener("click", start);
+
+function start() {
+    console.log(",Funca?")
+      // Typed Initiate
+      if ($('.hero .hero-text h2').length == 1) {
+        var typed_strings = ",You will now be given, 20 words of a random song, Try to guess the song's title!";
+        var typed = new Typed('.hero .hero-text h2', {
+            strings: typed_strings.split(','),
+            typeSpeed: 60,
+            backSpeed: 20,
+            loop: false,
+            showCursor: false
+        });
+    }
+}
